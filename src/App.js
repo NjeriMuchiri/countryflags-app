91cdacb058fc0246913a86264521bd04892d3d2c
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-02-25 19:42:21 +0300

    Added some new features on the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Container from './Pages/Container';
+import './App.css';
+import { useState,useEffect} from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const[continent,setContinent] = useState([]);
+  const[countries,setCountry] = useState('')
+   
+      useEffect(() =>{
+        const getData = async() => {
+          const data = await axios.get('https://restcountries.com/v3.1/all');
+          setContinent(data);
+        };
+        getData();
+      },[]);
+      console.log(continent);
+      
     return(
          <>
          <Container/>
+      <div className='country-container'>
+           
+           
+      </div>
       </>
     )
 }
--- src/Pages/Container.js
+++ src/Pages/Container.js
@@ -1,16 +1,30 @@
 import '../App.css';
 import React from 'react';
+import { useState } from 'react';
 
 const Container = () =>{
+    const[description,setDescription] = useState('Africa');
     return(
-        <>
         <body>
-           <div className="container">
-
-           </div> 
-        </body>
-           
-        </>
+          <section className='container-section'>
+              <h1 className='heading' >Continent Flags</h1>
+              <div className="section-continents">
+                  <label htmlFor="Continents">Choose a Continent: </label>
+                  <select value={description} 
+                  onChange = {(e) =>setDescription(e.target.value)}
+                  className="countries">
+                      <option value="Africa">Africa</option>
+                      <option value="Asia">Asia</option>
+                      <option value="Europe">Europe</option>
+                      <option value="North America">North America</option>
+                      <option value="South America">South America</option>
+                      <option value="Antartica">Antartica</option>
+                      <option value="Oceania">Oceania</option>
+                      
+                  </select>
+              </div>
+              </section> 
+           </body>
     );
 };
 export default Container;
