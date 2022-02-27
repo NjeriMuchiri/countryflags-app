cd1ee290a064c5754219fdc81b543b5d08414dd7
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-02-27 13:23:31 +0300

    Added new functionality

--- src/Pages/Container.js
+++ src/Pages/Container.js
@@ -4,42 +4,60 @@ import { useState,useEffect } from 'react';
 import axios from 'axios';
 
 const Container = () =>{
-    const[description,setDescription] = useState('Africa');
-    const[continent,setContinent] = useState([]);
+    // const[description,setDescription] = useState('Africa');
+    const[countries,setCountries] = useState([]);
+    const[continent,setContinent] = useState('');
 
-    const getCountries = () => {
-        axios.get('https://restcountries.com/v3.1/all').then((response) => {
-            console.log(response);
-            const country = response.data;
-            setContinent(country);
-        });
-    };
-      useEffect(() => getCountries(), [])
-           
+      const HandleChangeApi = async () =>{
+         axios.get(`https://restcountries.com/v3.1/region/ame`)
+         .then((response) =>{
+            //  let data = response.data;
+            console.log(response)
+         })
+         .catch((error)=>{
+             console.log(error);
+         });
+      }
+    // useEffect(() =>{
+    //     axios.get(`https://restcountries.com/v3.1/region/ame`)
+    //     .then(res => {
+    //         console.log(res);
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     })       
+    // },[]);
+        
     return(
-        <body>
+        <>
           <section className='container-section'>
               <h1 className='heading' >Continent Flags</h1>
               <div className="section-continents">
                   <label htmlFor="Continents">Choose a Continent: </label>
-                  <select value={description} 
-                  onChange = {(e) =>setDescription(e.target.value)}
-                  className="countries">
+                  <select 
+                  onChange = {HandleChangeApi}
+                //   value={continent}
+                  
+                // onChange ={(e) =>setContinent(e.target.value)
+                // }
+            >
                       <option value="Africa">Africa</option>
                       <option value="Asia">Asia</option>
                       <option value="Europe">Europe</option>
-                      <option value="North America">North America</option>
-                      <option value="South America">South America</option>
                       <option value="Antartica">Antartica</option>
                       <option value="Oceania">Oceania</option>
-                      
                   </select>
-              </div>
-              <div className='countries'>
-                 
-              </div>
+                  <div className='countries-display'>
+                  {continent.length !== 0 && countries.map(country => 
+                      <div key={country.area}>  
+                      <img src={country.flag.svg} alt={country.name.common} />  
+                      <h4>{country.name.common}</h4>          
+                          </div>
+                  )}
+                  </div>
+                    </div>
               </section> 
-           </body>
+           </>
     );
 };
 export default Container;
